6efd5f3d236e3ab59193d86ae1eaa21d7f4323e7
jcastro506 <castrojoshua805.com>
2021-01-11 16:58:54 -0500

    commit tree 5f57beaf37736452bf4950826bf9aaa529fedd1f

---
+++ src/components/Links.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function links (){
+    return (
+        <h3>Links</h3>
+        <a href={links.github}></a>
+        <a href={links.linkedn}> </a>
+    )
+}
+
+export default Links;
+
